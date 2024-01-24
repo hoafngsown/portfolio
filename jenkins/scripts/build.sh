000c803db171bbf1d1ec2e7e80609b4440b64858
Xơn Phạm Hoàng <xonhoang@Xons-MacBook-Air.local>
2024-01-24 13:04:15 +0700

    fix: attach tag to docker

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -7,8 +7,8 @@ docker ps -a
 
 docker build -t portfolio:latest .
 
-docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
+docker tag portfolio:latest hoafngxown/portfolio:latest
 
-docker image ls
+docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 
 docker push hoafngxown/portfolio:latest 
